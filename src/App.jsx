ef51e3ac2f8b84334fe56ec55c15c79d2a475860
maxkdavis <7098417+maxkdavis@users.noreply.github.com>
2025-03-18 07:43:23 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react';
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg';
+import './styles/App.css';
+import HeroContent from '../src/components/heroContent';
+
+function App() {
+  return (
+    <>
+      <HeroContent />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/heroContent.jsx
@@ -0,0 +1,36 @@
+function HeroContent() {
+  return (
+    <>
+      <section className='section--hero'>
+        <div className='hero--header-wrap'>
+          <h1 className='hero--heading-primary'>
+            Get <span className='highlight---purple'>insights</span> that help your business grow.
+          </h1>
+          <p className='hero--text'>
+            Discover the benefits of data analytics and make better decisions regarding revenue, customer experience,
+            and overall efficiency.
+          </p>
+        </div>
+        <div className='hero--stats-wrap'>
+          <div className='stats'>
+            <h2 className='stats-data-text'>10K+</h2>
+            <p className='stats-data-description'>Companies</p>
+          </div>
+          <div className='stats'>
+            <h2 className='stats-data-text'>314</h2>
+            <p className='stats-data-description'>Templates</p>
+          </div>
+          <div className='stats'>
+            <h2 className='stats-data-text'>12M+</h2>
+            <p className='stats-data-description'>Queries</p>
+          </div>
+        </div>
+      </section>
+      <section className='section--banner'>
+        <img src='../src/assets/images/image-header-desktop.jpg' className='banner-image' alt='' />
+      </section>
+    </>
+  );
+}
+
+export default HeroContent;
